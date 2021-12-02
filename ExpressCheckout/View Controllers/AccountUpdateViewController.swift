141bee5e84cde18cd905f0853680853b2a568fa2
nagalakshmiratnala77 <nagalakshmiratnala77@gmail.com>
2021-12-02 10:25:59 -0600

    updated account details

--- ExpressCheckout/View Controllers/AccountUpdateViewController.swift
+++ ExpressCheckout/View Controllers/AccountUpdateViewController.swift
@@ -14,8 +14,13 @@ class AccountUpdateViewController: UIViewController {
     
     @IBOutlet weak var firstNameValue: UITextField!
     @IBOutlet weak var lastNameValue: UITextField!
-    @IBOutlet weak var emailValue: UITextField!
+    
     @IBOutlet weak var phonenoValue: UITextField!
+    
+    @IBOutlet weak var addressLine1Value: UITextField!
+    @IBOutlet weak var cityValue: UITextField!
+    @IBOutlet weak var stateValue: UITextField!
+    @IBOutlet weak var zipcodeValue: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,19 +38,42 @@ class AccountUpdateViewController: UIViewController {
     
     
     @IBAction func updateData(_ sender: Any) {
+        if(firstNameValue.text == "")
+        {
+            createAlert(title: "First Name is empty!", msg: "Enter Valid First Name")
+            return
+            
+        }
+        if(lastNameValue.text == ""){
+            createAlert(title: "Last Name is empty!", msg: "Enter Valid Last Name")
+            return
+            
+        }
+        if(phonenoValue.text == ""){
+            createAlert(title: "Phone number is empty!", msg: "Enter Valid Phone number")
+            return
+            
+        }
+        
         let uid = Auth.auth().currentUser?.uid
         db.collection("users").document(uid!).updateData([
             "firstName": firstNameValue.text,
             "lastName": lastNameValue.text,
             "mblno": phonenoValue.text,
-            "email": emailValue.text
+            "addressLine1": addressLine1Value.text,
+            "city": cityValue.text,
+            "state": stateValue.text,
+            "zipcode": zipcodeValue.text
         ])
         
         createAlert(title: "Account Information Updated!", msg: "")
         firstNameValue.text!.removeAll()
         lastNameValue.text!.removeAll()
-        emailValue.text!.removeAll()
         phonenoValue.text!.removeAll()
+        addressLine1Value.text!.removeAll()
+        cityValue.text!.removeAll()
+        stateValue.text!.removeAll()
+        zipcodeValue.text!.removeAll()
         //self.dismiss(animated: true, completion: nil)
         //self.dismiss(animated: true, completion: {})
         
